2a193a13733fb313860b48c376f0ed9e446f97a8
a-lesneva <lesneva0805@gmail.com>
2020-08-31 11:56:47 +1000

    working version

--- website/website/settings.py
+++ website/website/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'schoolsdb.sqlite',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'website_local',
-    #     'USER': 'admin',
-    #     'PASSWORD': 'admin123',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'schoolsdb.sqlite',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'website_local',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 
