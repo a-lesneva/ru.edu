a59f9568f0d798b3d2eb4bb99ae56b42ddb10b94
a-lesneva <lesneva0805@gmail.com>
2020-09-04 10:32:01 +1000

    cleanup

--- python_scripts/school_json_fixture.py
+++
@@ -1,5 +0,0 @@
-import json
-
-f = open('corrected_list.json').read()
-data = json.loads(f)
-
--- website/website/settings.py
+++ website/website/settings.py
@@ -82,10 +82,6 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'schoolsdb.sqlite',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'website_local',
